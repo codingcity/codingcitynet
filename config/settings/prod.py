fad2ffdda3270bd6e079bbebd9bf78b019de0288
codingcity <codingcity@naver.com>
2023-06-22 13:52:21 +0900

    fm12

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,18 @@ ALLOWED_HOSTS = ['54.164.103.70','codingcity.net','www.codingcity.net']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+MEDIA_ROOT =  BASE_DIR / 'media/'
+"""
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+
+#for media
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'_media')
+
+"""
