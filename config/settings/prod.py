ba74cac485bae1d52ecb3beb5e49bee96fcf4aef
codingcity <codingcity@naver.com>
2023-06-22 14:20:49 +0900

    fm17

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['54.164.103.70','codingcity.net','www.codingcity.net']
+ALLOWED_HOSTS = ['54.164.103.70', 'codingcity.net', 'www.codingcity.net']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
