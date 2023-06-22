59fa131fcd83e964232c5382ea6eea130ea03dcf
codingcity <codingcity@naver.com>
2023-06-22 15:40:39 +0900

    footermodule

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['54.164.103.70', 'codingcity.net', 'www.codingcity.net']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+"""
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+"""
