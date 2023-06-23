7828a1fe1fee043976e24c1a7a4bf1d8a08ff127
codingcity <codingcity@naver.com>
2023-06-23 17:39:09 +0900

    boardsBlogImgFilesShowOk

--- boards/forms.py
+++ boards/forms.py
@@ -5,7 +5,7 @@ from boards.models import Question, Answer
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question  # 사용할 모델
-        fields = ['subject', 'content', 'file_upload']  # QuestionForm에서 사용할 Question 모델의 속성
+        fields = ['subject', 'content', 'head_image']#, 'file_upload']  # QuestionForm에서 사용할 Question 모델의 속성
         """
         widgets = {
             'subject': forms.TextInput(attrs={'class': 'form-control'}),
@@ -15,7 +15,8 @@ class QuestionForm(forms.ModelForm):
         labels = {
             'subject': '제목',
             'content': '내용',
-            'file_upload': '파일',
+            'head_image': '이미지',
+            #'file_upload': '파일',
         }
 
 
